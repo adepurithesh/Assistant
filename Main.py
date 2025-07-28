1656d3531b47f7d16e7f9934df361074df58210e
adepurithesh <ritheshadep@gmail.com>
2025-07-28 22:38:19 +0530

    Import Keys

--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
 import pandas as pd
+import openai
+import os
+from dotenv import load_dotenv,find_dotenv
+
+_ = load_dotenv(find_dotenv())
+openai.api_Key = os.environ['api_key']
+print(os.environ['api_key'])
+print("Hello")
